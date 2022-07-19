54124ea7fb2c9fdbfb290b140d04043d61eaa1f5
haodeng <hao.deng.m@gmail.com>
2022-07-20 00:28:57 +0200

    more examples

---
+++ part4/rot13-reader.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (r13 rot13Reader) Read(b []byte) (int, error) {
+
+	n, err := r13.r.Read(b)
+	if err == nil {
+		for i, v := range b {
+			b[i] = rot13(v)
+		}
+	}
+
+	return n, err
+}
+
+func rot13(b byte) byte {
+	if b >= 'A' && b <= 'Z' {
+		b = 'A' + (b-'A'+13)%26
+	} else if b >= 'a' && b <= 'z' {
+		b = 'a' + (b-'a'+13)%26
+	}
+	return b
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
