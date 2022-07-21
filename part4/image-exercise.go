9fc1659f4305593f5b3908385286e2e2a5c7b7b9
haodeng <hao.deng.m@gmail.com>
2022-07-21 10:49:01 +0200

    more examples

---
+++ part4/image-exercise.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"image"
+	"image/color"
+)
+
+type Image struct{}
+
+func (i Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, 100, 100)
+}
+
+func (i Image) At(x, y int) color.Color {
+	v := uint8(x * y)
+	return color.RGBA{v, v, 255, 255}
+}
+
+func main() {
+	m := Image{}
+	m.At(300, 400)
+}
