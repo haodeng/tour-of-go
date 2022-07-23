540fafd77310fce389560358c72133cac07231dc
haodeng <hao.deng.m@gmail.com>
2022-07-24 00:40:23 +0200

    more examples

--- part5/generic-type.go
+++ part5/generic-type.go
@@ -9,10 +9,33 @@ type List[T any] struct {
 	val  T
 }
 
+func (lst *List[T]) add(v T) {
+
+	for lst.next != nil {
+		lst = lst.next
+	}
+
+	lst.next = &List[T]{val: v}
+}
+
+func (lst *List[T]) GetAll() []T {
+	var elems []T
+	for e := lst; e != nil; e = e.next {
+		elems = append(elems, e.val)
+	}
+	return elems
+}
+
 func main() {
 	t := List[int]{val: 1}
 	t.next = &List[int]{val: 2}
 
 	fmt.Println(t.val)
 	fmt.Println(t.next.val)
+
+	lst := List[int]{val: 1}
+	lst.add(10)
+	lst.add(13)
+	lst.add(23)
+	fmt.Println("list:", lst.GetAll())
 }
