5d0f33007b1ff5fef4e8fb681ca2a822de224ac7
haodeng <hao.deng.m@gmail.com>
2022-08-11 21:38:28 +0200

    add type switch

--- part2/switch.go
+++ part2/switch.go
@@ -31,4 +31,23 @@ func main() {
 	default:
 		fmt.Println("Good evening.")
 	}
+
+	var t2 interface{}
+	t2 = functionOfSomeType()
+	switch t := t2.(type) {
+	default:
+		fmt.Printf("unexpected type %T\n", t) // %T prints whatever type t has
+	case bool:
+		fmt.Printf("boolean %t\n", t) // t has type bool
+	case int:
+		fmt.Printf("integer %d\n", t) // t has type int
+	case *bool:
+		fmt.Printf("pointer to boolean %t\n", *t) // t has type *bool
+	case *int:
+		fmt.Printf("pointer to integer %d\n", *t) // t has type *int
+	}
+}
+
+func functionOfSomeType() int {
+	return 1
 }
